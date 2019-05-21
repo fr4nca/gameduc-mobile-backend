c2373a4963575a42cd64dbbaa9baa25bbdcd0d19
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-21 15:26:05 -0300

    apapap

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.use("/", (req, res) => {
   res.status(404).send("Route not found");
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
